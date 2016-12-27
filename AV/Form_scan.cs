8e5db04e92a596c40f70b49987ee10ccd4fe9ead
x4m <pro4minh@gmail.com>
2016-12-27 19:08:43 +0700

    27/12_ form scan

--- AV/Form_scan.cs
+++ AV/Form_scan.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,7 +26,10 @@ namespace AV
             InitializeComponent();
             using (var compiler = new Compiler())
             {
-                compiler.AddRuleFile(".\\Rule\\Rule1.yara");
+                String[] Scan_RuleFile = Directory.GetFiles(".//Rules");
+                foreach (String rulefile in Scan_RuleFile)
+                   compiler.AddRuleFile(".//Rules//Rule1.yara");
+
                 Scan_rule = compiler.GetRules();
             }
         }
